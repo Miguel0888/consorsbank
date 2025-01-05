350c93c68147ddc6ab1468bfcf59a455486ca016
Miguel Angelo Fernández Iglesias <82086544+mfernandezig@users.noreply.github.com>
2025-01-05 05:57:51 +0100

    V4

--- src/main/java/consorsbank/api/SecureMarketDataService.java
+++ src/main/java/consorsbank/api/SecureMarketDataService.java
@@ -1,42 +1,63 @@
 package consorsbank.api;
 
+import com.consorsbank.module.tapi.grpc.AccessServiceGrpc;
+import com.consorsbank.module.tapi.grpc.SecurityServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.netty.shaded.io.grpc.netty.GrpcSslContexts;
-import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
 import io.grpc.netty.shaded.io.grpc.netty.NegotiationType;
+import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
 
 import javax.net.ssl.SSLException;
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
-import java.util.concurrent.TimeUnit;
 
 public class SecureMarketDataService {
+
     private final ManagedChannel channel;
+    private final AccessServiceGrpc.AccessServiceBlockingStub accessServiceStub;
+    private final SecurityServiceGrpc.SecurityServiceStub securityServiceStub;
 
-    /**
-     * Konstruktor für den SecureMarketDataService
-     *
-     * @param host             Hostname des gRPC-Servers
-     * @param port             Port des gRPC-Servers
-     * @param certContent       Root-Zertifikat
-     * @throws SSLException    Falls SSL-Initialisierung fehlschlägt
-     */
-    public SecureMarketDataService(String host, int port, String certContent) throws SSLException {
-//        File certFile = new File(trustCertPath);
-        InputStream certInputStream = new ByteArrayInputStream(certContent.getBytes());
+    public SecureMarketDataService(String host, int port, Object certSource) throws SSLException {
+        InputStream certStream = resolveCertificate(certSource);
+        this.channel = initChannel(host, port, certStream);
+        this.accessServiceStub = AccessServiceGrpc.newBlockingStub(channel);
+        this.securityServiceStub = SecurityServiceGrpc.newStub(channel);
+    }
 
-        this.channel = NettyChannelBuilder.forAddress(host, port)
-                .negotiationType(NegotiationType.TLS) // HTTP/2 mit TLS
-                .sslContext(GrpcSslContexts.forClient().trustManager(certInputStream).build()) // Zertifikat laden
+    private InputStream resolveCertificate(Object certSource) {
+        if (certSource instanceof File) {
+            try {
+                return new FileInputStream((File) certSource);
+            } catch (Exception e) {
+                throw new IllegalArgumentException("Fehler beim Lesen der Zertifikatsdatei.", e);
+            }
+        } else if (certSource instanceof InputStream) {
+            return (InputStream) certSource;
+        } else if (certSource instanceof String) {
+            return new ByteArrayInputStream(((String) certSource).getBytes());
+        } else {
+            throw new IllegalArgumentException("Ungültige Zertifikatsquelle. Erlaubt sind File, InputStream oder String.");
+        }
+    }
+
+    private ManagedChannel initChannel(String host, int port, InputStream certStream) throws SSLException {
+        return NettyChannelBuilder.forAddress(host, port)
+                .negotiationType(NegotiationType.TLS)
+                .sslContext(GrpcSslContexts.forClient().trustManager(certStream).build())
                 .build();
     }
 
-    public ManagedChannel getChannel() {
-        return channel;
+    public AccessServiceGrpc.AccessServiceBlockingStub getAccessServiceStub() {
+        return accessServiceStub;
     }
 
-    public void close() throws InterruptedException {
-        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    public SecurityServiceGrpc.SecurityServiceStub getSecurityServiceStub() {
+        return securityServiceStub;
+    }
+
+    public void close() {
+        channel.shutdown();
     }
 }
--- src/main/java/consorsbank/controllers/MainController.java
+++ src/main/java/consorsbank/controllers/MainController.java
@@ -1,19 +1,14 @@
 package consorsbank.controllers;
 
-import com.consorsbank.module.tapi.grpc.AccessServiceGrpc;
-import com.consorsbank.module.tapi.grpc.SecurityServiceGrpc;
 import com.consorsbank.module.tapi.grpc.access.LoginReply;
 import com.consorsbank.module.tapi.grpc.access.LoginRequest;
 import com.consorsbank.module.tapi.grpc.security.SecurityCode;
 import com.consorsbank.module.tapi.grpc.security.SecurityCodeType;
-import com.consorsbank.module.tapi.grpc.security.SecurityMarketDataReply;
 import com.consorsbank.module.tapi.grpc.security.SecurityMarketDataRequest;
 import com.consorsbank.module.tapi.grpc.security.SecurityWithStockExchange;
 import com.consorsbank.module.tapi.grpc.stock.StockExchange;
 import com.consorsbank.module.tapi.observers.MarketDataDataObserver;
 import consorsbank.api.SecureMarketDataService;
-import io.grpc.ManagedChannel;
-import io.grpc.stub.StreamObserver;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
@@ -61,21 +56,11 @@ public class MainController {
             String secret = "123456";
 
             // Initialisiere den SecureMarketDataService
-//            SecureMarketDataService marketService = new SecureMarketDataService(
-//                    "localhost", // Host
-//                    40443,       // Port
-//                    "src/main/resources/roots.pem" // Pfad zum Zertifikat
-//            );
-            SecureMarketDataService marketService = new SecureMarketDataService(
-                    "localhost", 40443, certContent);
-
+            SecureMarketDataService marketService = new SecureMarketDataService("localhost", 40443, certContent);
 
             // Login durchführen
-            ManagedChannel channel = marketService.getChannel();
-            AccessServiceGrpc.AccessServiceBlockingStub accessServiceStub = AccessServiceGrpc.newBlockingStub(channel);
-
             LoginRequest loginRequest = LoginRequest.newBuilder().setSecret(secret).build();
-            LoginReply loginReply = accessServiceStub.login(loginRequest);
+            LoginReply loginReply = marketService.getAccessServiceStub().login(loginRequest);
 
             if (loginReply.getError() != null && !loginReply.getError().equals(com.consorsbank.module.tapi.grpc.common.Error.getDefaultInstance())) {
                 txtOutput.appendText("Login fehlgeschlagen: " + loginReply.getError().getMessage() + "\n");
@@ -86,34 +71,13 @@ public class MainController {
             txtOutput.appendText("Login erfolgreich! Access-Token erhalten.\n");
 
             // Eingaben für den SecurityMarketDataRequest
-            String securityCodeStr = "710000"; // Beispiel-WKN
-            SecurityCodeType securityCodeType = SecurityCodeType.WKN;
-            String stockExchangeId = "OTC";
-            String currency = "EUR";
+            SecurityCode securityCode = SecurityCode.newBuilder().setCode("710000").setCodeType(SecurityCodeType.WKN).build();
+            StockExchange stockExchange = StockExchange.newBuilder().setId("OTC").build();
+            SecurityWithStockExchange securityWithStockExchange = SecurityWithStockExchange.newBuilder().setSecurityCode(securityCode).setStockExchange(stockExchange).build();
 
-            SecurityCode securityCode = SecurityCode.newBuilder()
-                    .setCode(securityCodeStr)
-                    .setCodeType(securityCodeType)
-                    .build();
+            SecurityMarketDataRequest request = SecurityMarketDataRequest.newBuilder().setAccessToken(accessToken).setSecurityWithStockexchange(securityWithStockExchange).setCurrency("EUR").build();
 
-            StockExchange stockExchange = StockExchange.newBuilder()
-                    .setId(stockExchangeId)
-                    .build();
-
-            SecurityWithStockExchange securityWithStockExchange = SecurityWithStockExchange.newBuilder()
-                    .setSecurityCode(securityCode)
-                    .setStockExchange(stockExchange)
-                    .build();
-
-            SecurityMarketDataRequest request = SecurityMarketDataRequest.newBuilder()
-                    .setAccessToken(accessToken)
-                    .setSecurityWithStockexchange(securityWithStockExchange)
-                    .setCurrency(currency)
-                    .build();
-
-            SecurityServiceGrpc.SecurityServiceStub securityServiceStub = SecurityServiceGrpc.newStub(channel);
-
-            MarketDataDataObserver observer = new MarketDataDataObserver(request, securityServiceStub);
+            MarketDataDataObserver observer = new MarketDataDataObserver(request, marketService.getSecurityServiceStub());
             observer.onCompleted();
 
             txtOutput.appendText("Marktdaten-Stream abgeschlossen.\n");
@@ -122,5 +86,4 @@ public class MainController {
             txtOutput.appendText("Fehler beim Laden der Marktdaten: " + e.getMessage() + "\n");
         }
     }
-
 }
